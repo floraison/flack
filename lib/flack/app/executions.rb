39d3434d616335a7fff86f14152e08940574fdc6
Tsunehisa DOI <dmicky@nifty.com>
2017-09-14 09:57:24 +0900

    Modify executions controller to get a execution

--- lib/flack/app/executions.rb
+++ lib/flack/app/executions.rb
@@ -3,16 +3,27 @@
 #
 class Flack::App
 
+  # GET /executions
   def get_executions(env)
 
 # TODO implement paging
     respond(env, @unit.executions.all)
   end
 
+  # GET /executions/<id>
   def get_executions_i(env)
 
-# TODO
-    debug_respond(env)
+    exe = @unit.executions[env['flack.args'][0]]
+    return respond_not_found(env) unless exe
+    respond(env, exe)
+  end
+
+  # GET /executions/<exid>
+  def get_executions_s(env)
+
+    exe = @unit.executions.where(exid: env['flack.args'][0]).last
+    return respond_not_found(env) unless exe
+    respond(env, exe)
   end
 end
 
